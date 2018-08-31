6c37ea00ca04e8ed58264310c2c1a568a47ab8c5
Kevin <ketsang12@gmail.com>
2018-08-31 11:25:43 -0400

    helper functions finished for stack

---
+++ helper.js
@@ -0,0 +1,25 @@
+const peek = stack => {
+  return stack.top.data;
+};
+
+const display = stack => {
+  // return stack.top;
+  const stackDisplay = {};
+  let currNode = stack.top;
+  let position = 0;
+  while (currNode) {
+    // add node to list obj
+    stackDisplay[`node${position}`] = 
+    { value: currNode.data, next: currNode.next ? currNode.next.data: null };
+    // move to the next node
+    currNode = currNode.next;
+    position++;
+  }
+  // console.log(stackDisplay);
+  return stackDisplay;
+};
+
+module.exports = {
+  peek,
+  display
+};
--- stack-and-q-drills.js
+++ stack-and-q-drills.js
@@ -2,13 +2,21 @@ const Stack = require('./stack-class');
 
 const starTrek = new Stack();
 
+const {
+  peek,
+  display
+} = require('./helper');
+
+
 function main (){
   starTrek.push('Scotty');
-  console.log(JSON.stringify(starTrek));
+  // console.log(JSON.stringify(starTrek));
   starTrek.push('McCoy');
   starTrek.push('Spock');
   starTrek.push('Kirk');
-  console.log(JSON.stringify(starTrek, null, 2));
+  // console.log(JSON.stringify(starTrek, null, 2));
+  // console.log(peek(starTrek));
+  console.log(display(starTrek));
 }
 
 main();
